b28ad0a96a76141edd77b2cb3fe6c04743332033
Savas Vedova <savas.vedova@gmail.com>
2017-10-08 21:29:25 +0200

    Helper function to filter non propTypes props

---
+++ src/helpers/functions.js
@@ -0,0 +1,17 @@
+/**
+ * This function will remove the properties that are already defined in propTypes and return
+ * the undefined ones. This way we can directly filter props and pass the extra props to the root element in
+ * a component.
+ *
+ * @param {Object} propTypes The list of defined properties.
+ * @param {Object} props The instance props.
+ */
+export const filterProps = (propTypes, props) => {
+  const filteredProps = {}
+
+  Object.keys(props)
+    .filter(k => propTypes[k] === undefined)
+    .forEach(k => filteredProps[k] = props[k])
+
+  return filteredProps
+}
