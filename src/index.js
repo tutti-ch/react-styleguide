30696e1679771ba5de47692ce1cbdbe892b77752
Savas Vedova <savas.vedova@gmail.com>
2017-10-12 17:48:10 +0200

    Map category icons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
+/**
+ * This file will get exported.
+ */
 export Image from "components/Image";
 export Spinner from "components/Spinner";
 export Table from "components/Table";
 export Button from "components/Button";
+
+export const icons = {
+  categories: {
+    "1000": require('./styles/Icons/assets/category/immobilien.svg'),
+    "4020": require('./styles/Icons/assets/category/antiquitaten.svg'),
+    "6200": require('./styles/Icons/assets/category/baby.svg'),
+    "4200": require('./styles/Icons/assets/category/buecher.svg'),
+    "6800": require('./styles/Icons/assets/category/buro.svg'),
+    "5000": require('./styles/Icons/assets/category/computer.svg'),
+    "2000": require('./styles/Icons/assets/category/fahrzeuge.svg'),
+    "4120": require('./styles/Icons/assets/category/filme.svg'),
+    "5600": require('./styles/Icons/assets/category/foto.svg'),
+    "3200": require('./styles/Icons/assets/category/garten.svg'),
+    "3000": require('./styles/Icons/assets/category/haushalt.svg'),
+    "6600": require('./styles/Icons/assets/category/kleidung.svg'),
+    "4300": require('./styles/Icons/assets/category/musik.svg'),
+    "4160": require('./styles/Icons/assets/category/sammeln.svg'),
+    "3400": require('./styles/Icons/assets/category/spielzeuge.svg'),
+    "4000": require('./styles/Icons/assets/category/sport.svg'),
+    "6400": require('./styles/Icons/assets/category/stellen.svg'),
+    "5800": require('./styles/Icons/assets/category/telefon.svg'),
+    "9080": require('./styles/Icons/assets/category/tickets.svg'),
+    "8000": require('./styles/Icons/assets/category/tiere.svg'),
+    "5400": require('./styles/Icons/assets/category/tv.svg'),
+    "7020": require('./styles/Icons/assets/category/sonstiges.svg')
+  }
+}
