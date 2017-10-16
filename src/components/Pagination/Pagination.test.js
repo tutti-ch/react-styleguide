240b04b08bf041695abecf36703ed0d9dddcdcc6
Marcello Romanelli <marcelloromanelli@me.com>
2017-10-16 17:22:34 +0200

    Add tests for Pagination component

--- src/components/Pagination/Pagination.test.js
+++ src/components/Pagination/Pagination.test.js
@@ -7,12 +7,51 @@ import toJson from "enzyme-to-json";
 describe("(Component) Pagination", () => {
   test("should match the snapshot", () => {
     const comp = mount(
-      <Spinner
-        size={Spinner.SIZE_LARGE}
-        color={Spinner.COLOR_DARK}
-        className="my-class"
-      />
+      <Pagination page={302} totalItems={3020} pageText="Page" />
     );
     expect(toJson(comp)).toMatchSnapshot();
+  });
+
+  test("should not show any page if there are no items", () => {
+    const comp = mount(<Pagination page={1} totalItems={0} pageText="Page" />);
+    expect(comp.find("Page").length).toBe(0);
+  });
+
+  test("should show less pages than the bucket size", () => {
+    const comp = mount(
+      <Pagination page={1} totalItems={15} pageBucket={5} pageText="Page" />
+    );
+
+    // Start, Left, 1, 2, Right, End = 6
+    expect(comp.find("Page").length).toBe(6);
+  });
+
+  test("should show as many pages as a bucket can contain", () => {
+    const comp = mount(
+      <Pagination page={1} totalItems={15000} pageBucket={5} pageText="Page" />
+    );
+
+    // Start, Left, 1, 2, 3, 4, 5, Right, End = 9
+    expect(comp.find("Page").length).toBe(9);
+  });
+
+  test("should show the right bucket", () => {
+    const comp = mount(
+      <Pagination page={6} totalItems={15000} pageBucket={5} pageText="Page" />
+    );
+
+    // Start, Left, 1, 2, 3, 4, 5, Right, End = 9
+    expect(comp.find("Page").map(p => p.prop("number"))).toEqual([
+      1,
+      5,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      7,
+      1500
+    ]);
   });
 });
