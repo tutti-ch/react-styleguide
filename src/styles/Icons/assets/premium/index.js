57da24db3f444ffb54f3484162abd87223f23062
circleci <circleci@tutti.ch>
2017-11-01 12:34:03 +0000

    Deploy to GitHub pages [ci skip]

--- src/styles/Icons/assets/premium/index.js
+++ src/styles/Icons/assets/premium/index.js
@@ -1,5 +1,5 @@
-export SellFaster from "./sell-faster.svg"
 export Highlight from "./highlight.svg"
-export Bump from "./bump.svg"
 export Combo from "./combo.svg"
+export SellFaster from "./sell-faster.svg"
 export Gallery from "./gallery.svg"
+export Bump from "./bump.svg"
