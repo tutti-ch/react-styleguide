88c54e614e625958aac67bf6f374c63dcd4f7f91
Adam Juras <adam@tutti.ch>
2017-11-17 10:31:15 +0100

    Resort files

---
+++ src/styles/Icons/assets/premium/index.js
@@ -0,0 +1,5 @@
+export Bump from "./bump.svg";
+export Highlight from "./highlight.svg";
+export Gallery from "./gallery.svg";
+export Combo from "./combo.svg";
+export SellFaster from "./sell-faster.svg";
