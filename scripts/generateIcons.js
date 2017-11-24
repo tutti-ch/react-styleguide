352d9a472764d9ef7ae4f015462f9f536bc09c1c
Savas Vedova <savas.vedova@gmail.com>
2017-11-24 11:00:50 +0100

    Generate ico-* classes

--- scripts/generateIcons.js
+++ scripts/generateIcons.js
@@ -1,69 +1,62 @@
-console.log(__dirname)
-const src = __dirname + "/../src/styles"
-const fs = require("fs")
-const path = require("path")
+const src = __dirname + "/../src/styles";
+const fs = require("fs");
+const path = require("path");
+const glob = require("glob");
+const svgo = require("svgo");
+const WebfontsGenerator = require("webfonts-generator");
 
-/**
- * Walk recursively and list all files.
- * @param dir
- * @param done
- */
-const walk = (dir, done) => {
-  let results = []
-
-  fs.readdir(dir, (err, list) => {
-    if (err) {
-      return done(err)
-    }
-
-    let i = 0;
-
-    (function next() {
-      let file = list[i++]
-
-      if (!file) {
-        return done(null, results)
+glob(src + "/**/*.svg", (err, files) => {
+  WebfontsGenerator(
+    {
+      files,
+      fontName: "TuttiFont",
+      css: true,
+      cddDest: src + "/styles/Typography/fonts",
+      dest: src + "/Typography/fonts",
+      templateOptions: {
+        classPrefix: "ico-",
+        baseSelector: ".ico"
       }
-
-      file = dir + "/" + file
-      fs.stat(file, function (err, stat) {
-        if (stat && stat.isDirectory()) {
-          walk(file, function (err, res) {
-            results = results.concat(res)
-            next()
-          })
-        } else {
-          results.push(file)
-          next()
-        }
-      })
-    })()
-  })
-}
+    },
+    (error, result) => {
+      if (error) {
+        console.log("Failed while generating icons!", error);
+      } else {
+        console.log("Generated fonts!");
+      }
+    }
+  );
+});
 
 const toCamelCase = str => {
-  return str[0].toUpperCase() + str.substr(1).replace(/[-_]([a-z])/g, g => g[1].toUpperCase()).replace('.svg', '')
-}
+  return (
+    str[0].toUpperCase() +
+    str
+      .substr(1)
+      .replace(/[-_]([a-z])/g, g => g[1].toUpperCase())
+      .replace(".svg", "")
+  );
+};
 
-walk(src, (err, results) => {
-  let folders = {}
+glob(src, (err, results) => {
+  let folders = {};
 
   results.forEach(file => {
     if (/\.svg$/.test(file)) {
-      const pieces = file.split("/")
-      const fname = pieces.pop()
-      const path = pieces.join("/") + "/index.js"
+      const pieces = file.split("/");
+      const fname = pieces.pop();
+      const path = pieces.join("/") + "/index.js";
 
-      folders[path] = folders[path] || []
-      folders[path].push(`export ${toCamelCase(fname)} from "./${fname}"`)
+      folders[path] = folders[path] || [];
+      folders[path].push(`export ${toCamelCase(fname)} from "./${fname}"`);
     }
-  })
+  });
 
   Object.keys(folders).forEach(file => {
     fs.writeFile(file, folders[file].sort().join("\n"), err => {
       if (err === null) {
-        console.log("Created index file: " + file)
+        console.log("Created index file: " + file);
       }
-    })
-  })
-})
+    });
+  });
+});
