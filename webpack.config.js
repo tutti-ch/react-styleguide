2775b290b6f32041a0f326a8920accb5023124b9
Savas Vedova <savas.vedova@gmail.com>
2017-11-24 11:00:15 +0100

    Merge configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,69 @@
-module.exports = process.env.NODE_ENV === 'umd'
-  ? require('./config/webpack.umd.config')
-  : require('./config/webpack.dev.config')
+const path = require("path");
+
+const config = {
+  // The base directory for resolving the entry option
+  resolve: {
+    modules: [path.resolve("./node_modules")]
+  },
+
+  module: {
+    rules: [
+      // Babel loader, will use your project’s .babelrc
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+
+      // Helps importing files
+      {
+        test: /\.(png|gif|jpg|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      },
+
+      // Fonts
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: "file-loader?name=public/fonts/[name].[ext]"
+      },
+
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+      },
+
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            // creates style nodes from JS strings
+            loader: "style-loader"
+          },
+          {
+            // translates CSS into CommonJS
+            loader: "css-loader",
+            options: {
+              modules: true,
+              localIdentName: "[name]_[local]_[hash:base64:5]",
+              sourceMap: true,
+              camelCase: true
+            }
+          },
+          {
+            // compiles Sass to CSS
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  }
+};
+
+module.exports = config;
