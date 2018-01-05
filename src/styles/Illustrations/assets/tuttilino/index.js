1c36ee6312ec3d3526e32c390473bb861e349c9e
Savas Vedova <savas.vedova@gmail.com>
2018-01-05 10:32:02 +0100

    Add new images

--- src/styles/Illustrations/assets/tuttilino/index.js
+++ src/styles/Illustrations/assets/tuttilino/index.js
@@ -1,3 +1,4 @@
 export Tuttilino404 from "./tuttilino-404.png"
 export TuttilinoError from "./tuttilino-error.svg"
+export TuttilinoErrorMobile from "./tuttilino-error-mobile.png"
 export TuttilinoHey from "./tuttilino-hey.svg"
