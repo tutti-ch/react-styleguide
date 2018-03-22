c193d5cee9d347aae9376f745674dd61c123c89f
circleci <circleci@tutti.ch>
2018-03-22 15:24:05 +0000

    Deploy to GitHub pages [ci skip]

--- src/styles/Icons/assets/canton/index.js
+++ src/styles/Icons/assets/canton/index.js
@@ -1,45 +1,27 @@
 import React from "react";
 import Sprite from "../../_Sprite";
-export const CantonAargau = <Sprite viewBox="0 0 35 35" id="canton-aargau" />;
-export const CantonAll = <Sprite viewBox="0 0 35 35" id="canton-all" />;
-export const CantonAppenzell = (
-  <Sprite viewBox="0 0 35 35" id="canton-appenzell" />
-);
-export const CantonBasel = <Sprite viewBox="0 0 35 35" id="canton-basel" />;
-export const CantonBern = <Sprite viewBox="0 0 35 35" id="canton-bern" />;
-export const CantonFreiburg = (
-  <Sprite viewBox="0 0 35 35" id="canton-freiburg" />
-);
-export const CantonGenf = <Sprite viewBox="0 0 35 35" id="canton-genf" />;
-export const CantonGlarus = <Sprite viewBox="0 0 35 35" id="canton-glarus" />;
-export const CantonGraubunden = (
-  <Sprite viewBox="0 0 35 35" id="canton-graubunden" />
-);
-export const CantonJura = <Sprite viewBox="0 0 35 35" id="canton-jura" />;
-export const CantonLiechtenstein = (
-  <Sprite viewBox="0 0 35 35" id="canton-liechtenstein" />
-);
-export const CantonLuzern = <Sprite viewBox="0 0 35 35" id="canton-luzern" />;
-export const CantonNeuenburg = (
-  <Sprite viewBox="0 0 35 35" id="canton-neuenburg" />
-);
-export const CantonNiedobwalden = (
-  <Sprite viewBox="0 0 35 35" id="canton-niedobwalden" />
-);
-export const CantonSchaffhausen = (
-  <Sprite viewBox="0 0 35 35" id="canton-schaffhausen" />
-);
-export const CantonSchwyz = <Sprite viewBox="0 0 35 35" id="canton-schwyz" />;
-export const CantonSolothurn = (
-  <Sprite viewBox="0 0 35 35" id="canton-solothurn" />
-);
-export const CantonStgallen = (
-  <Sprite viewBox="0 0 35 35" id="canton-stgallen" />
-);
-export const CantonTessin = <Sprite viewBox="0 0 35 35" id="canton-tessin" />;
-export const CantonThurgau = <Sprite viewBox="0 0 35 35" id="canton-thurgau" />;
-export const CantonUri = <Sprite viewBox="0 0 35 35" id="canton-uri" />;
-export const CantonWaadt = <Sprite viewBox="0 0 35 35" id="canton-waadt" />;
-export const CantonWallis = <Sprite viewBox="0 0 35 35" id="canton-wallis" />;
-export const CantonZug = <Sprite viewBox="0 0 35 35" id="canton-zug" />;
-export const CantonZurich = <Sprite viewBox="0 0 35 35" id="canton-zurich" />;
+export const CantonAargau = <Sprite viewBox="0 0 35 35" id="canton-aargau"/>
+export const CantonAll = <Sprite viewBox="0 0 35 35" id="canton-all"/>
+export const CantonAppenzell = <Sprite viewBox="0 0 35 35" id="canton-appenzell"/>
+export const CantonBasel = <Sprite viewBox="0 0 35 35" id="canton-basel"/>
+export const CantonBern = <Sprite viewBox="0 0 35 35" id="canton-bern"/>
+export const CantonFreiburg = <Sprite viewBox="0 0 35 35" id="canton-freiburg"/>
+export const CantonGenf = <Sprite viewBox="0 0 35 35" id="canton-genf"/>
+export const CantonGlarus = <Sprite viewBox="0 0 35 35" id="canton-glarus"/>
+export const CantonGraubunden = <Sprite viewBox="0 0 35 35" id="canton-graubunden"/>
+export const CantonJura = <Sprite viewBox="0 0 35 35" id="canton-jura"/>
+export const CantonLiechtenstein = <Sprite viewBox="0 0 35 35" id="canton-liechtenstein"/>
+export const CantonLuzern = <Sprite viewBox="0 0 35 35" id="canton-luzern"/>
+export const CantonNeuenburg = <Sprite viewBox="0 0 35 35" id="canton-neuenburg"/>
+export const CantonNiedobwalden = <Sprite viewBox="0 0 35 35" id="canton-niedobwalden"/>
+export const CantonSchaffhausen = <Sprite viewBox="0 0 35 35" id="canton-schaffhausen"/>
+export const CantonSchwyz = <Sprite viewBox="0 0 35 35" id="canton-schwyz"/>
+export const CantonSolothurn = <Sprite viewBox="0 0 35 35" id="canton-solothurn"/>
+export const CantonStgallen = <Sprite viewBox="0 0 35 35" id="canton-stgallen"/>
+export const CantonTessin = <Sprite viewBox="0 0 35 35" id="canton-tessin"/>
+export const CantonThurgau = <Sprite viewBox="0 0 35 35" id="canton-thurgau"/>
+export const CantonUri = <Sprite viewBox="0 0 35 35" id="canton-uri"/>
+export const CantonWaadt = <Sprite viewBox="0 0 35 35" id="canton-waadt"/>
+export const CantonWallis = <Sprite viewBox="0 0 35 35" id="canton-wallis"/>
+export const CantonZug = <Sprite viewBox="0 0 35 35" id="canton-zug"/>
+export const CantonZurich = <Sprite viewBox="0 0 35 35" id="canton-zurich"/>
--- src/styles/Icons/assets/subscriptions/index.js
+++ src/styles/Icons/assets/subscriptions/index.js
@@ -1,3 +1,3 @@
 import React from "react";
 import Sprite from "../../_Sprite";
-export const LockHexagon = <Sprite viewBox="0 0 6.9 8.1" id="lock-hexagon" />;
+export const LockHexagon = <Sprite viewBox="0 0 6.9 8.1" id="lock-hexagon"/>
