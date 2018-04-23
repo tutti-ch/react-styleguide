04cf47359bbb1057f33dffc027ebbba65013f690
circleci <circleci@tutti.ch>
2018-04-23 09:59:16 +0000

    Deploy to GitHub pages [ci skip]

--- src/styles/Icons/assets/subscriptions/index.js
+++ src/styles/Icons/assets/subscriptions/index.js
@@ -1,3 +1,5 @@
 import React from "react";
 import Sprite from "../../_Sprite";
 export const LockHexagon = <Sprite viewBox="0 0 6.9 8.1" id="lock-hexagon"/>
+export const SubscriptionsPlus = <Sprite viewBox="0 0 26.5 30" id="subscriptions-plus"/>
+export const SubscriptionsPro = <Sprite viewBox="0 0 26.5 30" id="subscriptions-pro"/>
