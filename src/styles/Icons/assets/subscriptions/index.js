d213298182944221c27a58e63ca79bd07515acc6
circleci <circleci@tutti.ch>
2018-04-27 13:14:56 +0000

    Deploy to GitHub pages [ci skip]

--- src/styles/Icons/assets/subscriptions/index.js
+++ src/styles/Icons/assets/subscriptions/index.js
@@ -1,5 +1,21 @@
 import React from "react";
 import Sprite from "../../_Sprite";
 export const LockHexagon = <Sprite viewBox="0 0 6.9 8.1" id="lock-hexagon"/>
+export const SubscriptionsPlusBadge = <Sprite viewBox="0 0 160 160" id="subscriptions-plus-badge"/>
+export const SubscriptionsPlusBump = <Sprite viewBox="0 0 160 160" id="subscriptions-plus-bump"/>
+export const SubscriptionsPlusEye = <Sprite viewBox="0 0 160 160" id="subscriptions-plus-eye"/>
+export const SubscriptionsPlusHexagon = <Sprite viewBox="0 0 160 160" id="subscriptions-plus-hexagon"/>
+export const SubscriptionsPlusImages = <Sprite viewBox="0 0 160 160" id="subscriptions-plus-images"/>
+export const SubscriptionsPlusNoAds = <Sprite viewBox="0 0 160 160" id="subscriptions-plus-no_ads"/>
 export const SubscriptionsPlus = <Sprite viewBox="0 0 26.5 30" id="subscriptions-plus"/>
+export const SubscriptionsProBadge = <Sprite viewBox="0 0 160 160" id="subscriptions-pro-badge"/>
+export const SubscriptionsProBump = <Sprite viewBox="0 0 160 160" id="subscriptions-pro-bump"/>
+export const SubscriptionsProEye = <Sprite viewBox="0 0 160 160" id="subscriptions-pro-eye"/>
+export const SubscriptionsProHexagon = <Sprite viewBox="0 0 160 160" id="subscriptions-pro-hexagon"/>
+export const SubscriptionsProImages = <Sprite viewBox="0 0 160 160" id="subscriptions-pro-images"/>
+export const SubscriptionsProNoAds = <Sprite viewBox="0 0 160 160" id="subscriptions-pro-no_ads"/>
+export const SubscriptionsProNotifications = <Sprite viewBox="0 0 160 160" id="subscriptions-pro-notifications"/>
+export const SubscriptionsProRenew = <Sprite viewBox="0 0 160 160" id="subscriptions-pro-renew"/>
+export const SubscriptionsProSupport = <Sprite viewBox="0 0 160 160" id="subscriptions-pro-support"/>
+export const SubscriptionsProWeb = <Sprite viewBox="0 0 160 160" id="subscriptions-pro-web"/>
 export const SubscriptionsPro = <Sprite viewBox="0 0 26.5 30" id="subscriptions-pro"/>
