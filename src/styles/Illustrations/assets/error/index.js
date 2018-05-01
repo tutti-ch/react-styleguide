110b5a1766c50a284c422cc05b9b3b7752d94088
circleci <circleci@tutti.ch>
2018-05-01 13:18:17 +0000

    Deploy to GitHub pages [ci skip]

--- src/styles/Illustrations/assets/error/index.js
+++ src/styles/Illustrations/assets/error/index.js
@@ -1,4 +1,4 @@
 import React from "react";
 import Image from "../../../../components/Image"
-export const myadsEmpty = p => <Image {...p} src={require("./myads-empty.svg")} />
-export const page404 = p => <Image {...p} src={require("./tutti-404-page-design-2015.svg")} />
+export const MyadsEmpty = p => <Image {...p} src={require("./myads-empty.svg")} />
+export const Tutti404PageDesign2015 = p => <Image {...p} src={require("./tutti-404-page-design-2015.svg")} />
