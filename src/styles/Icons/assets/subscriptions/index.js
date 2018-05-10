503260ffd723055066d33f732711c1e655230710
Sergi Gomez <sergi@tutti.ch>
2018-05-10 17:13:30 +0200

    Index file for new icons

--- src/styles/Icons/assets/subscriptions/index.js
+++ src/styles/Icons/assets/subscriptions/index.js
@@ -1,23 +1,73 @@
 import React from "react";
-import Image from "../../../../components/Image"
-import Sprite from "../../_Sprite"
-export const LockHexagon = p => <Sprite {...p} viewBox="0 0 6.9 8.1" id="lock-hexagon"/>
-export const SubscriptionsPlusBadge = p => <Image {...p} src={require("./subscriptions-plus-badge.svg")} />
-export const SubscriptionsPlusBump = p => <Image {...p} src={require("./subscriptions-plus-bump.svg")} />
-export const SubscriptionsPlusEye = p => <Image {...p} src={require("./subscriptions-plus-eye.svg")} />
-export const SubscriptionsPlusHexagon = p => <Image {...p} src={require("./subscriptions-plus-hexagon.svg")} />
-export const SubscriptionsPlusImages = p => <Image {...p} src={require("./subscriptions-plus-images.svg")} />
-export const SubscriptionsPlusNoAds = p => <Image {...p} src={require("./subscriptions-plus-no_ads.svg")} />
-export const SubscriptionsPlus = p => <Sprite {...p} viewBox="0 0 26.5 30" id="subscriptions-plus"/>
-export const SubscriptionsProBadge = p => <Image {...p} src={require("./subscriptions-pro-badge.svg")} />
-export const SubscriptionsProBump = p => <Image {...p} src={require("./subscriptions-pro-bump.svg")} />
-export const SubscriptionsProEye = p => <Image {...p} src={require("./subscriptions-pro-eye.svg")} />
-export const SubscriptionsProHexagon = p => <Image {...p} src={require("./subscriptions-pro-hexagon.svg")} />
-export const SubscriptionsProImages = p => <Image {...p} src={require("./subscriptions-pro-images.svg")} />
-export const SubscriptionsProImmo = p => <Image {...p} src={require("./subscriptions-pro-immo.svg")} />
-export const SubscriptionsProNoAds = p => <Image {...p} src={require("./subscriptions-pro-no_ads.svg")} />
-export const SubscriptionsProNotifications = p => <Image {...p} src={require("./subscriptions-pro-notifications.svg")} />
-export const SubscriptionsProRenew = p => <Image {...p} src={require("./subscriptions-pro-renew.svg")} />
-export const SubscriptionsProSupport = p => <Image {...p} src={require("./subscriptions-pro-support.svg")} />
-export const SubscriptionsProWeb = p => <Image {...p} src={require("./subscriptions-pro-web.svg")} />
-export const SubscriptionsPro = p => <Sprite {...p} viewBox="0 0 26.5 30" id="subscriptions-pro"/>
+import Image from "../../../../components/Image";
+import Sprite from "../../_Sprite";
+export const LockHexagon = p => (
+  <Sprite {...p} viewBox="0 0 6.9 8.1" id="lock-hexagon" />
+);
+export const SubscriptionsPlusBadge = p => (
+  <Image {...p} src={require("./subscriptions-plus-badge.svg")} />
+);
+export const SubscriptionsPlusBump = p => (
+  <Image {...p} src={require("./subscriptions-plus-bump.svg")} />
+);
+export const SubscriptionsPlusEye = p => (
+  <Image {...p} src={require("./subscriptions-plus-eye.svg")} />
+);
+export const SubscriptionsPlusHexagon = p => (
+  <Image {...p} src={require("./subscriptions-plus-hexagon.svg")} />
+);
+export const SubscriptionsPlusImages = p => (
+  <Image {...p} src={require("./subscriptions-plus-images.svg")} />
+);
+export const SubscriptionsPlusNoAds = p => (
+  <Image {...p} src={require("./subscriptions-plus-no_ads.svg")} />
+);
+export const SubscriptionsPlus = p => (
+  <Sprite {...p} viewBox="0 0 26.5 30" id="subscriptions-plus" />
+);
+export const SubscriptionsProBadge = p => (
+  <Image {...p} src={require("./subscriptions-pro-badge.svg")} />
+);
+export const SubscriptionsProBump = p => (
+  <Image {...p} src={require("./subscriptions-pro-bump.svg")} />
+);
+export const SubscriptionsProEye = p => (
+  <Image {...p} src={require("./subscriptions-pro-eye.svg")} />
+);
+export const SubscriptionsProHexagon = p => (
+  <Image {...p} src={require("./subscriptions-pro-hexagon.svg")} />
+);
+export const SubscriptionsProImages = p => (
+  <Image {...p} src={require("./subscriptions-pro-images.svg")} />
+);
+export const SubscriptionsProImmo = p => (
+  <Image {...p} src={require("./subscriptions-pro-immo.svg")} />
+);
+export const SubscriptionsProNoAds = p => (
+  <Image {...p} src={require("./subscriptions-pro-no_ads.svg")} />
+);
+export const SubscriptionsProNotifications = p => (
+  <Image {...p} src={require("./subscriptions-pro-notifications.svg")} />
+);
+export const SubscriptionsProRenew = p => (
+  <Image {...p} src={require("./subscriptions-pro-renew.svg")} />
+);
+export const SubscriptionsProSupport = p => (
+  <Image {...p} src={require("./subscriptions-pro-support.svg")} />
+);
+export const SubscriptionsProWeb = p => (
+  <Image {...p} src={require("./subscriptions-pro-web.svg")} />
+);
+export const SubscriptionsProApproval = p => (
+  <Image {...p} src={require("./subscriptions-approval.svg")} />
+);
+export const SubscriptionsProReach = p => (
+  <Image {...p} src={require("./subscriptions-reach.svg")} />
+);
+export const SubscriptionsProPerformance = p => (
+  <Image {...p} src={require("./subscriptions-performance.svg")} />
+);
+
+export const SubscriptionsPro = p => (
+  <Sprite {...p} viewBox="0 0 26.5 30" id="subscriptions-pro" />
+);
