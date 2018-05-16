1d9164272b41bcea7e96e930fe81ccf988925922
Sergi Gomez <sergi@tutti.ch>
2018-05-16 11:54:02 +0200

    Removed fill atribute for transparent icons. Added bump

--- src/styles/Icons/assets/premium/index.js
+++ src/styles/Icons/assets/premium/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Sprite from "../../_Sprite"
+export const BumpTransparent = p => <Sprite {...p} viewBox="0 0 160 160" id="bump-transparent"/>
 export const Bump = p => <Sprite {...p} viewBox="0 0 490 490" id="bump"/>
 export const Bump1 = p => <Sprite {...p} viewBox="0 0 300 300" id="bump1"/>
 export const Bump2 = p => <Sprite {...p} viewBox="0 0 300 300" id="bump2"/>
