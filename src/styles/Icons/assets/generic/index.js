975e5889d75983f277e001365ff3b6a2b4b671ef
Sergi Gomez <sergi@tutti.ch>
2018-07-27 13:30:19 +0200

    Logo updated

--- src/styles/Icons/assets/generic/index.js
+++ src/styles/Icons/assets/generic/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Sprite from "../../_Sprite"
 export const Archive = p => <Sprite {...p} viewBox="0 0 30 30" id="archive"/>
+export const ArrowCircularRight = p => <Sprite {...p} viewBox="0 0 30 30" id="arrow-circular-right"/>
+export const ArrowDown = p => <Sprite {...p} viewBox="0 0 30 14" id="arrow-down"/>
 export const ArrowEnd = p => <Sprite {...p} viewBox="0 0 15.4 30" id="arrow-end"/>
+export const ArrowLeft = p => <Sprite {...p} viewBox="0 0 14 30" id="arrow-left"/>
 export const ArrowLink = p => <Sprite {...p} viewBox="0 0 30 30" id="arrow-link"/>
+export const ArrowRight = p => <Sprite {...p} viewBox="0 0 14 30" id="arrow-right"/>
 export const ArrowStart = p => <Sprite {...p} viewBox="0 0 15.4 30" id="arrow-start"/>
+export const Blackpoint = p => <Sprite {...p} viewBox="0 0 24 24" id="blackpoint"/>
 export const BubbleSpeech = p => <Sprite {...p} viewBox="0 0 30 30" id="bubble-speech"/>
+export const BubbleSpeech = p => <Sprite {...p} viewBox="0 0 30 30" id="bubble_speech"/>
 export const Call = p => <Sprite {...p} viewBox="0 0 30 30" id="call"/>
 export const Categories = p => <Sprite {...p} viewBox="0 0 30 30" id="categories"/>
+export const CheckCircle = p => <Sprite {...p} viewBox="0 0 30 30" id="check-circle"/>
+export const Check = p => <Sprite {...p} viewBox="0 0 30 30" id="check"/>
 export const ClockTransparent = p => <Sprite {...p} viewBox="0 0 53 53" id="clock-transparent"/>
 export const Clock = p => <Sprite {...p} viewBox="0 0 53 53" id="clock"/>
 export const CloseTags = p => <Sprite {...p} viewBox="0 0 5.9 5.9" id="close-tags"/>
@@ -29,9 +37,12 @@ export const Infinite = p => <Sprite {...p} viewBox="0 0 51 23" id="infinite"/>
 export const Info = p => <Sprite {...p} viewBox="0 0 30 30" id="info"/>
 export const InsertAd = p => <Sprite {...p} viewBox="0 0 49.8 49.8" id="insert-ad"/>
 export const InsertImage = p => <Sprite {...p} viewBox="0 0 30 30" id="insert-image"/>
+export const Label = p => <Sprite {...p} viewBox="0 0 32 32" id="label"/>
+export const LockHexagon = p => <Sprite {...p} viewBox="0 0 13.8 16.3" id="lock-hexagon"/>
 export const Lock = p => <Sprite {...p} viewBox="0 0 2.6 3.5" id="lock"/>
 export const Login = p => <Sprite {...p} viewBox="0 0 35 30" id="login"/>
 export const Logout = p => <Sprite {...p} viewBox="0 0 30 30" id="logout"/>
+export const Mail = p => <Sprite {...p} viewBox="0 0 30 30" id="mail"/>
 export const Menu = p => <Sprite {...p} viewBox="0 0 24 24" id="menu"/>
 export const MyItems = p => <Sprite {...p} viewBox="0 0 30 30" id="my-items"/>
 export const Next = p => <Sprite {...p} viewBox="0 0 30 30" id="next"/>
@@ -41,6 +52,10 @@ export const Pencil = p => <Sprite {...p} viewBox="0 0 30 30" id="pencil"/>
 export const Phone = p => <Sprite {...p} viewBox="0 0 30 30" id="phone"/>
 export const Pin = p => <Sprite {...p} viewBox="0 0 9.9 14.8" id="pin"/>
 export const Play = p => <Sprite {...p} viewBox="0 0 32 32" id="play"/>
+export const Plus = p => <Sprite {...p} viewBox="0 0 24 24" id="plus"/>
+export const PremiumBump = p => <Sprite {...p} viewBox="0 0 30 24.9" id="premium-bump"/>
+export const PremiumDbump = p => <Sprite {...p} viewBox="0 0 30 25" id="premium-dbump"/>
+export const PremiumWbump = p => <Sprite {...p} viewBox="0 0 30 25" id="premium-wbump"/>
 export const Prev = p => <Sprite {...p} viewBox="0 0 30 30" id="prev"/>
 export const PriceDown = p => <Sprite {...p} viewBox="0 0 30 30" id="price-down"/>
 export const PriceUp = p => <Sprite {...p} viewBox="0 0 30 30" id="price-up"/>
@@ -53,6 +68,7 @@ export const SavedSearchesOffAdd = p => <Sprite {...p} viewBox="0 0 49.8 51.9" i
 export const SavedSearches = p => <Sprite {...p} viewBox="0 0 48 48" id="saved-searches"/>
 export const Search = p => <Sprite {...p} viewBox="0 0 30 30" id="search"/>
 export const Select = p => <Sprite {...p} viewBox="0 0 30 30" id="select"/>
+export const SellFaster = p => <Sprite {...p} viewBox="0 0 20 20" id="sell_faster"/>
 export const ShareIos = p => <Sprite {...p} viewBox="0 0 29.5 31" id="share-ios"/>
 export const Share = p => <Sprite {...p} viewBox="0 0 24 24" id="share"/>
 export const Shield = p => <Sprite {...p} viewBox="0 0 16.5 20" id="shield"/>
@@ -67,7 +83,9 @@ export const TickCircle = p => <Sprite {...p} viewBox="0 0 30 30" id="tick-circl
 export const TickEmpty = p => <Sprite {...p} viewBox="0 0 30 30" id="tick-empty"/>
 export const TickSolid = p => <Sprite {...p} viewBox="0 0 20 20" id="tick-solid"/>
 export const Tick = p => <Sprite {...p} viewBox="0 0 30 30" id="tick"/>
+export const Trash = p => <Sprite {...p} viewBox="0 0 32 32" id="trash"/>
 export const TuttiCube = p => <Sprite {...p} viewBox="0 0 32 32" id="tutti-cube"/>
+export const TuttiPremium = p => <Sprite {...p} viewBox="0 0 32 32" id="tutti-premium"/>
 export const User = p => <Sprite {...p} viewBox="0 0 50 50" id="user"/>
 export const ViewBig = p => <Sprite {...p} viewBox="0 0 19 16.5" id="view-big"/>
 export const ViewList = p => <Sprite {...p} viewBox="0 0 19 18.3" id="view-list"/>
