9182658693d911f411cf72adb6ac5b3f3a66a830
Sergi Gomez <sergi@tutti.ch>
2018-08-29 15:08:46 +0200

    2 remanining old logos deleted

--- src/styles/Logos/assets/tutti/index.js
+++ src/styles/Logos/assets/tutti/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Image from "../../../../components/Image"
-export const LogoIosApp = p => <Image {...p} src={require("./logo-ios-app.svg")} />
-export const LogoWhite = p => <Image {...p} src={require("./logo-white.svg")} />
 export const TuttiEmailPlaceholder = p => <Image {...p} src={require("./tutti-email_placeholder.png")} />
 export const TuttiLogoNegative = p => <Image {...p} src={require("./tutti-logo-negative.svg")} />
 export const TuttiLogoSymbolNegative = p => <Image {...p} src={require("./tutti-logo-symbol-negative.svg")} />
