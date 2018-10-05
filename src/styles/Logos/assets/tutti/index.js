0a0725e21864b29cc55d560a7a97b809d93671bb
circleci <circleci@tutti.ch>
2018-10-05 12:01:56 +0000

    Deploy to GitHub pages [ci skip]

--- src/styles/Logos/assets/tutti/index.js
+++ src/styles/Logos/assets/tutti/index.js
@@ -4,6 +4,7 @@ export const TuttiEmailPlaceholder = p => <Image {...p} src={require("./tutti-em
 export const TuttiLogoNegative = p => <Image {...p} src={require("./tutti-logo-negative.svg")} />
 export const TuttiLogoSymbolNegative = p => <Image {...p} src={require("./tutti-logo-symbol-negative.svg")} />
 export const TuttiLogoSymbol = p => <Image {...p} src={require("./tutti-logo-symbol.svg")} />
+export const TuttiLogoVerticalSmall = p => <Image {...p} src={require("./tutti-logo-vertical-small.svg")} />
 export const TuttiLogoVertical = p => <Image {...p} src={require("./tutti-logo-vertical.svg")} />
 export const TuttiLogo = p => <Image {...p} src={require("./tutti-logo.svg")} />
 export const TuttiMail = p => <Image {...p} src={require("./tutti-mail.png")} />
