8d076c3735f023ab668885f3acdda4e68826dfa1
Marko Kostovski <marko.kostovski@outlook.com>
2018-10-24 16:44:54 +0200

    removing google plus icon

--- src/styles/Icons/assets/social_media/index.js
+++ src/styles/Icons/assets/social_media/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Sprite from "../../_Sprite"
 export const Facebook = p => <Sprite {...p} viewBox="0 0 30 30" id="facebook"/>
-export const GooglePlus = p => <Sprite {...p} viewBox="0 0 30 30" id="google-plus"/>
 export const Instagram = p => <Sprite {...p} viewBox="0 0 30 30" id="instagram"/>
 export const ShareFacebook = p => <Sprite {...p} viewBox="0 0 50 50" id="share-facebook"/>
 export const ShareMessenger = p => <Sprite {...p} viewBox="0 0 50 50" id="share-messenger"/>
 export const ShareWhatsapp = p => <Sprite {...p} viewBox="0 0 50 50" id="share-whatsapp"/>
 export const SocialFacebook = p => <Sprite {...p} viewBox="0 0 30 30" id="social-facebook"/>
-export const SocialGplus = p => <Sprite {...p} viewBox="0 0 30 30" id="social-gplus"/>
 export const SocialInstagram = p => <Sprite {...p} viewBox="0 0 30 30" id="social-instagram"/>
 export const SocialRss = p => <Sprite {...p} viewBox="0 0 30 30" id="social-rss"/>
 export const SocialTwitter = p => <Sprite {...p} viewBox="0 0 30 30" id="social-twitter"/>
