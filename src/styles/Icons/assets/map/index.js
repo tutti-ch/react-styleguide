6d4d7f25d5f2604c439ba53a413517a39d48065c
Sergi Gomez <sergi@tutti.ch>
2018-12-12 16:07:29 +0100

    Visited pin added

--- src/styles/Icons/assets/map/index.js
+++ src/styles/Icons/assets/map/index.js
@@ -3,4 +3,5 @@ import Sprite from "../../_Sprite"
 export const PinClusterSelected = p => <Sprite {...p} viewBox="0 0 30 39.4" id="pin-cluster-selected"/>
 export const PinCluster = p => <Sprite {...p} viewBox="0 0 30 39.4" id="pin-cluster"/>
 export const PinSingleSelected = p => <Sprite {...p} viewBox="0 0 30 39.4" id="pin-single-selected"/>
+export const PinSingleVisited = p => <Sprite {...p} viewBox="0 0 30 39.4" id="pin-single-visited"/>
 export const PinSingle = p => <Sprite {...p} viewBox="0 0 30 39.4" id="pin-single"/>
