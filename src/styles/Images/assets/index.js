99cce04e245e15e7be3d7c990d93b58767430569
circleci <circleci@tutti.ch>
2018-12-12 15:35:11 +0000

    Deploy to GitHub pages [ci skip]

--- src/styles/Images/assets/index.js
+++ src/styles/Images/assets/index.js
@@ -1,3 +1,3 @@
-export default {
-    BackgroundPlus: require("./backgrounds-plus.jpg")
-}
+import React from "react";
+import Image from "../../../components/Image"
+export const BackgroundsPlus = p => <Image {...p} src={require("./backgrounds-plus.jpg")} />
