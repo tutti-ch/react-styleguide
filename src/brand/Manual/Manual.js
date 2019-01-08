8dc02d530fd989dfab1c453091ec3b557d839b15
Adam Juras <adam@tutti.ch>
2019-01-08 17:14:10 +0100

    Bump to React 16.7

--- src/brand/Manual/Manual.js
+++ src/brand/Manual/Manual.js
@@ -8,11 +8,11 @@ const Manual = () => {
       <p style={{ marginBottom: "2rem" }}>
         The Brand manual define the identity of tutti.ch and gives instructions
         on how to use it. Download file in{" "}
-        <a class="rsg--link-37" href={pdf}>
+        <a className="rsg--link-37" href={pdf}>
           pdf
         </a>{" "}
         or{" "}
-        <a class="rsg--link-37" href={illustrator}>
+        <a className="rsg--link-37" href={illustrator}>
           Illustrator
         </a>
       </p>
