0ccfe66223399d0cf56ede7844704b73449df293
Erick Jones <erick.jones@gmail.com>
2019-08-05 11:25:10 +0200

    cleaning styleguide from old icon

--- src/styles/Icons/assets/premium/index.js
+++ src/styles/Icons/assets/premium/index.js
@@ -1,7 +1,6 @@
 // THIS FILE IS AUTO-GENERATED, DO NOT TOUCH.
 import Sprite from "../../_Sprite"
 import React from "react";
-export const BumpTransparent = p => <Sprite {...p} viewBox="0 0 160 160" id="bump-transparent"/>
 export const Bump = p => <Sprite {...p} viewBox="0 0 46 46" id="bump"/>
 export const Bump1 = p => <Sprite {...p} viewBox="0 0 32 32" id="bump1"/>
 export const Bump2 = p => <Sprite {...p} viewBox="0 0 32 32" id="bump2"/>
