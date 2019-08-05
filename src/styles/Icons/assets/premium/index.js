a699ddd5d2cea1658d1e5bf6b3fee187cd48c876
circleci <circleci@tutti.ch>
2019-08-05 07:39:34 +0000

    Deploy to GitHub pages [ci skip]

--- src/styles/Icons/assets/premium/index.js
+++ src/styles/Icons/assets/premium/index.js
@@ -2,10 +2,10 @@
 import Sprite from "../../_Sprite"
 import React from "react";
 export const BumpTransparent = p => <Sprite {...p} viewBox="0 0 160 160" id="bump-transparent"/>
-export const Bump = p => <Sprite {...p} viewBox="0 0 490 490" id="bump"/>
-export const Bump1 = p => <Sprite {...p} viewBox="0 0 300 300" id="bump1"/>
-export const Bump2 = p => <Sprite {...p} viewBox="0 0 300 300" id="bump2"/>
-export const Bump3 = p => <Sprite {...p} viewBox="0 0 300 300" id="bump3"/>
-export const Combo = p => <Sprite {...p} viewBox="0 0 1171.4 490" id="combo"/>
-export const Gallery = p => <Sprite {...p} viewBox="0 0 490 490" id="gallery"/>
-export const Highlight = p => <Sprite {...p} viewBox="0 0 490 490" id="highlight"/>
+export const Bump = p => <Sprite {...p} viewBox="0 0 46 46" id="bump"/>
+export const Bump1 = p => <Sprite {...p} viewBox="0 0 32 32" id="bump1"/>
+export const Bump2 = p => <Sprite {...p} viewBox="0 0 32 32" id="bump2"/>
+export const Bump3 = p => <Sprite {...p} viewBox="0 0 32 32" id="bump3"/>
+export const Combo = p => <Sprite {...p} viewBox="0 0 135 48" id="combo"/>
+export const Gallery = p => <Sprite {...p} viewBox="0 0 46 48" id="gallery"/>
+export const Highlight = p => <Sprite {...p} viewBox="0 0 46 46" id="highlight"/>
