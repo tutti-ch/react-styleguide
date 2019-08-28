be0e40f11e77c83809c3839b579472f53c844d1d
Sergi Gomez <sergi@tutti.ch>
2019-08-28 11:04:32 +0200

    Pin stroke increased

--- src/styles/Icons/assets/map/index.js
+++ src/styles/Icons/assets/map/index.js
@@ -2,4 +2,4 @@
 import Sprite from "../../_Sprite"
 import React from "react";
 export const PinCluster = p => <Sprite {...p} viewBox="0 0 30 39.4" id="pin-cluster"/>
-export const PinSingle = p => <Sprite {...p} viewBox="0 0 31.3 40" id="pin-single"/>
+export const PinSingle = p => <Sprite {...p} viewBox="0 0 24.6 32" id="pin-single"/>
