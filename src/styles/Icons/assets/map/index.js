7c0f28c0b61e827437b809001300cb0024019d46
Sergi Gomez <sergi@tutti.ch>
2019-08-28 09:25:08 +0200

    Map pointers changed to accept fill from css

--- src/styles/Icons/assets/map/index.js
+++ src/styles/Icons/assets/map/index.js
@@ -1,8 +1,5 @@
 // THIS FILE IS AUTO-GENERATED, DO NOT TOUCH.
 import Sprite from "../../_Sprite"
 import React from "react";
-export const PinClusterSelected = p => <Sprite {...p} viewBox="0 0 30 39.4" id="pin-cluster-selected"/>
 export const PinCluster = p => <Sprite {...p} viewBox="0 0 30 39.4" id="pin-cluster"/>
-export const PinSingleSelected = p => <Sprite {...p} viewBox="0 0 30 39.4" id="pin-single-selected"/>
-export const PinSingleVisited = p => <Sprite {...p} viewBox="0 0 30 39.4" id="pin-single-visited"/>
 export const PinSingle = p => <Sprite {...p} viewBox="0 0 30 39.4" id="pin-single"/>
