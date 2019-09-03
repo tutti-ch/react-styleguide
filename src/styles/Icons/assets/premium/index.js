4f946cf0d5fa2fff043107d8cd798c61b253010a
Erick Jones <erick.jones@gmail.com>
2019-09-03 12:37:57 +0200

    testing with another icon to check naming bug

--- src/styles/Icons/assets/premium/index.js
+++ src/styles/Icons/assets/premium/index.js
@@ -3,7 +3,6 @@ import Sprite from "../../_Sprite"
 import React from "react";
 export const Bump = p => <Sprite {...p} viewBox="0 0 46 46" id="bump"/>
 export const Combo = p => <Sprite {...p} viewBox="0 0 73 32" id="combo"/>
-export const Dbump = p => <Sprite {...p} viewBox="0 0 32 32" id="dbump"/>
+export const Dbump = p => <Sprite {...p} viewBox="0 0 24 24" id="dbump"/>
 export const Gallery = p => <Sprite {...p} viewBox="0 0 32 32" id="gallery"/>
 export const Highlight = p => <Sprite {...p} viewBox="0 0 46 46" id="highlight"/>
-export const Wbump = p => <Sprite {...p} viewBox="0 0 32 32" id="wbump"/>
