9fe921b99e3ff3ed02001e1311b6520081572424
Erick Jones <erick.jones@gmail.com>
2019-09-03 13:02:25 +0200

    regenerated the new sprite

--- src/styles/Icons/assets/premium/index.js
+++ src/styles/Icons/assets/premium/index.js
@@ -1,8 +1,6 @@
 // THIS FILE IS AUTO-GENERATED, DO NOT TOUCH.
 import Sprite from "../../_Sprite"
 import React from "react";
-export const Dbump = p => <Sprite {...p} viewBox="0 0 32 32" id="Dbump"/>
-export const Wbump = p => <Sprite {...p} viewBox="0 0 32 32" id="Wbump"/>
 export const Bump = p => <Sprite {...p} viewBox="0 0 46 46" id="bump"/>
 export const Combo = p => <Sprite {...p} viewBox="0 0 73 32" id="combo"/>
 export const Gallery = p => <Sprite {...p} viewBox="0 0 32 32" id="gallery"/>
