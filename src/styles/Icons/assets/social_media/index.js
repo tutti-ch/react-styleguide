55107ca096f1be99db82a836ad1881c65eb071fb
Erick Jones <erick.jones@gmail.com>
2019-09-12 13:38:06 +0200

    Updated the old icons for new versions and added some new as well.

--- src/styles/Icons/assets/social_media/index.js
+++ src/styles/Icons/assets/social_media/index.js
@@ -1,13 +1,12 @@
 // THIS FILE IS AUTO-GENERATED, DO NOT TOUCH.
+import Image from "../../../../components/Image"
 import Sprite from "../../_Sprite"
 import React from "react";
-export const Facebook = p => <Sprite {...p} viewBox="0 0 30 30" id="facebook"/>
-export const Instagram = p => <Sprite {...p} viewBox="0 0 30 30" id="instagram"/>
 export const ShareFacebook = p => <Sprite {...p} viewBox="0 0 50 50" id="share-facebook"/>
-export const ShareMessenger = p => <Sprite {...p} viewBox="0 0 50 50" id="share-messenger"/>
-export const ShareWhatsapp = p => <Sprite {...p} viewBox="0 0 50 50" id="share-whatsapp"/>
-export const SocialFacebook = p => <Sprite {...p} viewBox="0 0 30 30" id="social-facebook"/>
-export const SocialInstagram = p => <Sprite {...p} viewBox="0 0 30 30" id="social-instagram"/>
-export const SocialRss = p => <Sprite {...p} viewBox="0 0 30 30" id="social-rss"/>
-export const SocialTwitter = p => <Sprite {...p} viewBox="0 0 30 30" id="social-twitter"/>
-export const SocialYoutube = p => <Sprite {...p} viewBox="0 0 30 30" id="social-youtube"/>
+export const ShareMessenger = p => <Image {...p} src={require("./share-messenger.svg")} />
+export const ShareWhatsapp = p => <Sprite {...p} viewBox="0 0 24 24" id="share-whatsapp"/>
+export const SocialFacebook = p => <Sprite {...p} viewBox="0 0 24 24" id="social-facebook"/>
+export const SocialInstagram = p => <Sprite {...p} viewBox="0 0 24 24" id="social-instagram"/>
+export const SocialRss = p => <Sprite {...p} viewBox="0 0 24 24" id="social-rss"/>
+export const SocialTwitter = p => <Sprite {...p} viewBox="0 0 24 24" id="social-twitter"/>
+export const SocialYoutube = p => <Sprite {...p} viewBox="0 0 24 24" id="social-youtube"/>
