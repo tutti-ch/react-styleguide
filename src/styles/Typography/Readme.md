b55faa9183228e1861437a521a271f117b0a3a83
Erick Jones <erick.jones@gmail.com>
2019-10-04 17:13:26 +0200

    Added the Roboto Regular weight + fixed some stuff in the Styleguide.

--- src/styles/Typography/Readme.md
+++ src/styles/Typography/Readme.md
@@ -1,5 +1,7 @@
 Our main font is Roboto Light.
 
+*New*: Roboto Regular. Use "Roboto-Regular" to use it in your Style Sheets.
+
 Our heading font is Roboto Medium.
 
 ```jsx noeditor
--- src/styles/Typography/Typography.js
+++ src/styles/Typography/Typography.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "../style-guide.scss";
 
 const Typography = () => (
-  <div className={classes.wrapper}>
+  <div className={`${classes.wrapper} ${classes.typographySection}`}>
     <h1>Heading 1</h1>
     <h2>Heading 2</h2>
     <h3>Heading 3</h3>
@@ -10,7 +10,11 @@ const Typography = () => (
     <h5>Heading 5</h5>
     <h6>Heading 6</h6>
     <p>
-      This is a paragraph with <i>italic</i>, <u>underlined</u> and <b>bold</b>{" "}
+      This is a paragraph using "Roboto Light" (Currently our default font weight) with <i>italic</i>, <u>underlined</u> and <b>bold</b>{" "}
+      text.
+    </p>
+    <p className={classes.regularTypography}>
+      This is a paragraph using "Roboto Regular" with <i>italic</i>, <u>underlined</u> and <b>bold</b>{" "}
       text.
     </p>
   </div>
