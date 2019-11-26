fcdf8c421f9c560e79711ab5cad285d83ebd5973
Sergi Gomez <sergi@tutti.ch>
2019-11-26 15:30:08 +0100

    Logo t added

--- src/styles/Logos/assets/tutti/index.js
+++ src/styles/Logos/assets/tutti/index.js
@@ -1,12 +1,33 @@
 // THIS FILE IS AUTO-GENERATED, DO NOT TOUCH.
-import Image from "../../../../components/Image"
+import Image from "../../../../components/Image";
 import React from "react";
-export const TuttiEmailPlaceholder = p => <Image {...p} src={require("./tutti-email_placeholder.png")} />
-export const TuttiLogoNegative = p => <Image {...p} src={require("./tutti-logo-negative.svg")} />
-export const TuttiLogoSquare = p => <Image {...p} src={require("./tutti-logo-square.svg")} />
-export const TuttiLogoSymbolNegative = p => <Image {...p} src={require("./tutti-logo-symbol-negative.svg")} />
-export const TuttiLogoSymbol = p => <Image {...p} src={require("./tutti-logo-symbol.svg")} />
-export const TuttiLogoVerticalSmall = p => <Image {...p} src={require("./tutti-logo-vertical-small.svg")} />
-export const TuttiLogo = p => <Image {...p} src={require("./tutti-logo.svg")} />
-export const TuttiMail = p => <Image {...p} src={require("./tutti-mail.png")} />
-export const TuttiWatermark = p => <Image {...p} src={require("./tutti-watermark.png")} />
+export const TuttiEmailPlaceholder = p => (
+  <Image {...p} src={require("./tutti-email_placeholder.png")} />
+);
+export const TuttiLogoNegative = p => (
+  <Image {...p} src={require("./tutti-logo-negative.svg")} />
+);
+export const TuttiLogoSquare = p => (
+  <Image {...p} src={require("./tutti-logo-square.svg")} />
+);
+export const TuttiLogoT = p => (
+  <Image {...p} src={require("./tutti-logo-t.svg")} />
+);
+export const TuttiLogoSymbolNegative = p => (
+  <Image {...p} src={require("./tutti-logo-symbol-negative.svg")} />
+);
+export const TuttiLogoSymbol = p => (
+  <Image {...p} src={require("./tutti-logo-symbol.svg")} />
+);
+export const TuttiLogoVerticalSmall = p => (
+  <Image {...p} src={require("./tutti-logo-vertical-small.svg")} />
+);
+export const TuttiLogo = p => (
+  <Image {...p} src={require("./tutti-logo.svg")} />
+);
+export const TuttiMail = p => (
+  <Image {...p} src={require("./tutti-mail.png")} />
+);
+export const TuttiWatermark = p => (
+  <Image {...p} src={require("./tutti-watermark.png")} />
+);
